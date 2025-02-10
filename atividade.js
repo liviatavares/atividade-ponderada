4a8078402d240944a3869f9010a2d520c5aa19b8
Lívia Tavares <livia.tavares@sou.inteli.edu.br>
2025-02-10 11:56:26 -0300

    atividade

---
+++ atividade.js
@@ -0,0 +1,23 @@
+var nota1 = 9.0;
+var nota2 = 10.0;
+var nota3 = 7.0;
+var menorNota;
+var maiorNota;
+
+var media = (nota1 + nota2 + nota3)/3;
+
+
+if (media >= "7.0") {
+console.log("notas: " + nota1 + " | " + nota2 + " | " + nota3)
+console.log("média final: " + media)
+console.log("aprovado! :)")
+}
+
+
+else {
+    console.log("notas: " + nota1 + " | " +  nota2 + " | " + nota3)
+    console.log("média final: " + media)
+    console.log("reprovado! que pena :(")
+}
+
+
